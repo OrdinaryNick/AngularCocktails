5bf1c1f0e4dbc0504613700e415d72d48afcc172
Rostislav Novák <rostislav.novak92@gmail.com>
2018-07-22 15:39:34 +0200

    Connected all pages and services together.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
+import {AppRoutingModule} from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Services
+import {CocktailService} from './services/cocktail.service';
+
+// Components
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { EditCocktailComponent } from './pages/edit-cocktail/edit-cocktail.component';
+import { ShowCocktailComponent } from './pages/show-cocktail/show-cocktail.component';
+import { NewCocktailComponent } from './pages/new-cocktail/new-cocktail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    EditCocktailComponent,
+    ShowCocktailComponent,
+    NewCocktailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CocktailService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
