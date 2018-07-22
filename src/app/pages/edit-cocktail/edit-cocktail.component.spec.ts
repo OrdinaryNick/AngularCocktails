9a8fbd71b2e8963c1e5443b3a2a20663622f72ed
Rostislav Novák <rostislav.novak92@gmail.com>
2018-07-22 15:39:08 +0200

    Generated new cocktail and edit cocktail page.

---
+++ src/app/pages/edit-cocktail/edit-cocktail.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EditCocktailComponent } from './edit-cocktail.component';
+
+describe('EditCocktailComponent', () => {
+  let component: EditCocktailComponent;
+  let fixture: ComponentFixture<EditCocktailComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EditCocktailComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EditCocktailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/edit-cocktail/edit-cocktail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-cocktail',
+  templateUrl: './edit-cocktail.component.html',
+  styleUrls: ['./edit-cocktail.component.css']
+})
+export class EditCocktailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
